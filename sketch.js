d8a1ec74dc347b8ed64f687bd7cd9f0239d27bb3
Preetham Muscr <preethammuscr@users.noreply.github.com>
2017-11-24 19:44:18 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+var textfield;
+var output;
+var submit;
+
+function setup() {
+  noCanvas();
+  
+  textfield = select("#textarea");
+  textfield.input(newTyping);
+  output = select('#output');
+  submit = select("#submit");
+  submit.mousePressed(newText);
+
+}
+
+function newTyping() {
+  output.html(textfield.value());
+  
+}
+
+
+function newText() {
+  createP(textfield.value());
+}
